12fdeddba013114ee22119a9e498297e47add5eb
VioletGiraffe <alexxxx89@ya.ru>
2018-03-23 17:28:09 +0200

    tuple::visit_at helper added for run-time tuple indexing

--- tuple/tuple_helpers.hpp
+++ tuple/tuple_helpers.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <assert.h>
 #include <tuple>
 
 namespace detail {
@@ -22,5 +23,45 @@ namespace tuple {
 	template <typename Functor, class... Args>
 	void for_each(const Functor f, const std::tuple<Args...>& t) {
 		detail::apply(f, t, detail::tuple_iterator_tag<sizeof...(Args)>());
+	}
+
+	template <size_t I>
+	struct visit_impl
+	{
+		template <typename T, typename F>
+		static void visit(T& tup, size_t idx, F fun)
+		{
+			if (idx == I - 1) fun(std::get<I - 1>(tup));
+			else visit_impl<I - 1>::visit(tup, idx, fun);
+		}
+
+		template <typename T, typename F>
+		static void visit(const T& tup, size_t idx, F fun)
+		{
+			if (idx == I - 1) fun(std::get<I - 1>(tup));
+			else visit_impl<I - 1>::visit(tup, idx, fun);
+		}
+	};
+
+	template <>
+	struct visit_impl<0>
+	{
+		template <typename T, typename F>
+		static void visit(T& tup, size_t idx, F fun) { assert(false); }
+
+		template <typename T, typename F>
+		static void visit(const T& tup, size_t idx, F fun) { assert(false); }
+	};
+
+	template <typename F, typename... Ts>
+	void visit_at(const std::tuple<Ts...>& tup, size_t idx, F fun)
+	{
+		visit_impl<sizeof...(Ts)>::visit(tup, idx, fun);
+	}
+
+	template <typename F, typename... Ts>
+	void visit_at(std::tuple<Ts...>& tup, size_t idx, F fun)
+	{
+		visit_impl<sizeof...(Ts)>::visit(tup, idx, fun);
 	}
 }
