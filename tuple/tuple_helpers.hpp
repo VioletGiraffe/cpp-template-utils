11a79ba2ee23572daafcb8706d22b15b81fe749a
VioletGiraffe <alexxxx89@ya.ru>
2018-11-20 23:49:23 +0200

    tuple::for_each fixed

--- tuple/tuple_helpers.hpp
+++ tuple/tuple_helpers.hpp
@@ -2,67 +2,34 @@
 
 #include <assert.h>
 #include <tuple>
-
-namespace detail {
-	// A tag type based on size_t for indexing a tuple
-	template<size_t> struct tuple_iterator_tag{};
-
-	template <typename Functor, class Tuple, size_t pos>
-	void apply(const Functor f, const Tuple& t, tuple_iterator_tag<pos> ) {
-		f(std::get< std::tuple_size<Tuple>::value-pos >(t));
-		apply(f, t, tuple_iterator_tag<pos-1>());
-	}
-
-	template <typename Functor, class Tuple>
-	void apply(const Functor f, const Tuple& t, tuple_iterator_tag<1> ) {
-		f(std::get<std::tuple_size<Tuple>::value-1>(t));
-	}
-}
+#include <type_traits>
+#include <utility>
 
 namespace tuple {
-	template <typename Functor, class... Args>
-	void for_each(const Functor f, const std::tuple<Args...>& t) {
-		detail::apply(f, t, detail::tuple_iterator_tag<sizeof...(Args)>());
+	template<class F, class... Ts, std::size_t... Is>
+	void for_each(const std::tuple<Ts...>& tuple, F func, std::index_sequence<Is...>) {
+		using expander = int[];
+		(void)expander {
+			0, ((void)func(std::get<Is>(tuple)), 0)...
+		};
 	}
 
-	template <size_t I>
-	struct visit_impl
-	{
-		template <typename T, typename F>
-		static void visit(T& tup, size_t idx, F fun)
-		{
-			if (idx == I - 1) fun(std::get<I - 1>(tup));
-			else visit_impl<I - 1>::visit(tup, idx, fun);
-		}
-
-		template <typename T, typename F>
-		static void visit(const T& tup, size_t idx, F fun)
-		{
-			if (idx == I - 1) fun(std::get<I - 1>(tup));
-			else visit_impl<I - 1>::visit(tup, idx, fun);
-		}
-	};
-
-	template <>
-	struct visit_impl<0>
-	{
-		template <typename T, typename F>
-		static void visit(T& tup, size_t idx, F fun) { assert(false); }
-
-		template <typename T, typename F>
-		static void visit(const T& tup, size_t idx, F fun) { assert(false); }
-	};
-
-	template <typename F, typename... Ts>
-	void visit_at(const std::tuple<Ts...>& tup, size_t idx, F fun)
-	{
-		visit_impl<sizeof...(Ts)>::visit(tup, idx, fun);
+	template<class F, class... Ts>
+	void for_each(const std::tuple<Ts...>& tuple, F func) {
+		::tuple::for_each(tuple, func, std::make_index_sequence<sizeof... (Ts)>());
 	}
 
-	template <typename F, typename... Ts>
-	void visit_at(std::tuple<Ts...>& tup, size_t idx, F fun)
-	{
-		visit_impl<sizeof...(Ts)>::visit(tup, idx, fun);
+	template<class F, class... Ts, std::size_t... Is>
+	void for_each(std::tuple<Ts...>& tuple, F func, std::index_sequence<Is...>) {
+		using expander = int[];
+		(void)expander {
+			0, ((void)func(std::get<Is>(tuple)), 0)...
+		};
+	}
+
+	template<class F, class... Ts>
+	void for_each(std::tuple<Ts...>& tuple, F func) {
+		::tuple::for_each(tuple, func, std::make_index_sequence<sizeof... (Ts)>());
 	}
 }
 
--- utility/constexpr_algorithms.hpp
+++ utility/constexpr_algorithms.hpp
@@ -3,11 +3,11 @@
 #include <type_traits>
 
 template <int First, int Last, typename Functor>
-constexpr void static_for(const Functor& f)
+constexpr void static_for(Functor&& f)
 {
 	if constexpr (First < Last)
 	{
 		f(std::integral_constant<int, First>{});
-		static_for<First + 1, Last, Functor>(f);
+		static_for<First + 1, Last, Functor>(std::forward<Functor>(f));
 	}
 }
