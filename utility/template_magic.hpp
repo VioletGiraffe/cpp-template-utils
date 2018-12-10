45ba5c2c6e1070cefe1bf042e032b4b98937733f
VioletGiraffe <violet.giraffe@noreply.github.com>
2018-12-10 14:37:03 +0200

    remove_cv_and_reference_t

--- utility/template_magic.hpp
+++ utility/template_magic.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
+#include <type_traits>
+
 template <typename T>
 struct type_wrapper {
 	using type = T;
 };
+
+template <auto v>
+struct value_as_type {
+	static constexpr auto value = v;
+	using type = decltype(value);
+};
+
+template <typename T>
+using remove_cv_and_reference_t = std::remove_cv_t<std::remove_reference_t<std::remove_cv_t<T>>>;
