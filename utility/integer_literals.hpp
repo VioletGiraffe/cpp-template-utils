86438e74674ba7d885a9e52bcc337efe9639a5be
VioletGiraffe <alexxxx89@ya.ru>
2019-04-26 16:21:17 +0300

    u64 literal made constexpr

--- utility/integer_literals.hpp
+++ utility/integer_literals.hpp
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <type_traits>
 
-inline uint64_t operator "" _u64(unsigned long long int value)
+inline constexpr uint64_t operator "" _u64(unsigned long long int value)
 {
 	return static_cast<uint64_t>(value);
 }
