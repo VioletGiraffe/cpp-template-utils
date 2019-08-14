aba509c722ca1f1633635795f3990fd89265e6f2
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-08-14 15:06:37 +0300

    memory_cast improved

--- utility/memory_cast.hpp
+++ utility/memory_cast.hpp
@@ -1,18 +1,20 @@
 #pragma once
 
+#include <memory>
 #include <string.h>
 #include <type_traits>
+#include <utility>
 
 namespace util {
 
-template <typename TargetType>
-TargetType memory_cast(const void* const memoryPtr) noexcept
+template <typename TargetType, typename... TargetType_Constructor_Arguments>
+TargetType memory_cast(const void* const memoryPtr, TargetType_Constructor_Arguments&& ...constructorArguments) noexcept
 {
 	static_assert(!std::is_reference_v<TargetType>);
 	static_assert(std::is_trivial_v<TargetType>);
 
-	TargetType value;
-	::memcpy(&value, memoryPtr, sizeof(value));
+	TargetType value{std::forward<TargetType_Constructor_Arguments>(constructorArguments)...};
+	::memcpy(std::addressof(value), memoryPtr, sizeof(value));
 
 	return value;
 }
