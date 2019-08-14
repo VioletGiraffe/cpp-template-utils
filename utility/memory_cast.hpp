bc8650e39821cb1c5a26a8cda3547e91f5c7571a
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-08-14 14:52:30 +0300

    memory_cast.hpp

--- utility/memory_cast.hpp
+++ utility/memory_cast.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <type_traits>
 #include <string.h>
+#include <type_traits>
 
 namespace util {
 
@@ -9,7 +9,6 @@ template <typename TargetType>
 TargetType memory_cast(const void* const memoryPtr) noexcept
 {
 	static_assert(!std::is_reference_v<TargetType>);
-	static_assert(std::is_standard_layout_v<TargetType>);
 	static_assert(std::is_trivial_v<TargetType>);
 
 	TargetType value;
