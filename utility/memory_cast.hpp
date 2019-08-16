bc7f018f3cff33bd6a20c201f00b9d0fbc0da97e
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-08-16 20:48:16 +0300

    memory_cast: requirements for TargetType relaxed

--- utility/memory_cast.hpp
+++ utility/memory_cast.hpp
@@ -11,7 +11,7 @@ template <typename TargetType, typename... TargetType_Constructor_Arguments>
 TargetType memory_cast(const void* const memoryPtr, TargetType_Constructor_Arguments&& ...constructorArguments) noexcept
 {
 	static_assert(!std::is_reference_v<TargetType>);
-	static_assert(std::is_trivial_v<TargetType>);
+	static_assert(std::is_trivially_copyable_v<TargetType>);
 
 	TargetType value{std::forward<TargetType_Constructor_Arguments>(constructorArguments)...};
 	::memcpy(std::addressof(value), memoryPtr, sizeof(value));
