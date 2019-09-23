e536d0904f99f4a2ce751cf65600527dedbcc1b8
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-09-24 00:43:33 +0300

    More and better template magic!

--- utility/template_magic.hpp
+++ utility/template_magic.hpp
@@ -11,7 +11,17 @@ template <auto v>
 struct value_as_type {
 	static constexpr auto value = v;
 	using type = decltype(value);
+
+	constexpr operator type() const {
+		return v;
+	}
 };
 
 template <typename T>
 using remove_cv_and_reference_t = std::remove_cv_t<std::remove_reference_t<std::remove_cv_t<T>>>;
+
+template<class, template<class...> class>
+inline constexpr bool is_specialization_of = false;
+
+template<template<class...> class T, class... Args>
+inline constexpr bool is_specialization_of<T<Args...>, T> = true;
