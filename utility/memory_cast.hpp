7c329bb8cff06cfcf3fd66f9370961154d02e6d3
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-11-09 00:13:41 +0200

    memory_cast improved

--- utility/memory_cast.hpp
+++ utility/memory_cast.hpp
@@ -5,19 +5,22 @@
 #include <type_traits>
 #include <utility>
 
-namespace util {
-
-template <typename TargetType, typename... TargetType_Constructor_Arguments>
-constexpr TargetType memory_cast(const void* const memoryPtr, TargetType_Constructor_Arguments&& ...constructorArguments) noexcept
+template <typename TargetType, typename SourceType, typename... TargetType_Constructor_Arguments>
+constexpr TargetType memory_cast(SourceType source, TargetType_Constructor_Arguments&& ...constructorArguments) noexcept
 {
 	static_assert(!std::is_reference_v<TargetType>);
+	static_assert(!std::is_reference_v<SourceType>);
 	static_assert(std::is_same_v<TargetType, std::decay_t<TargetType>>);
 	static_assert(std::is_trivially_copyable_v<TargetType>);
 
 	TargetType value{std::forward<TargetType_Constructor_Arguments>(constructorArguments)...};
-	::memcpy(std::addressof(value), memoryPtr, sizeof(value));
+	if constexpr (std::is_pointer_v<SourceType>)
+		::memcpy(std::addressof(value), source, sizeof(value));
+	else
+	{
+		static_assert(sizeof(SourceType) == sizeof(TargetType));
+		::memcpy(std::addressof(value), std::addressof(source), sizeof(value));
+	}
 
 	return value;
-}
-
 }
