582eccb0e15d533241dd7abaab43a6fa6ce7c88e
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-01-16 14:30:01 +0200

    parameter_pack_helpers extended

--- parameter_pack/parameter_pack_helpers.hpp
+++ parameter_pack/parameter_pack_helpers.hpp
@@ -3,49 +3,62 @@
 #include "../utility/constexpr_algorithms.hpp"
 #include "../utility/template_magic.hpp"
 
+#include <optional>
 #include <tuple>
 #include <type_traits>
 
 namespace pack {
-	namespace detail {
-		// Index of the type in the list of types
-		template<size_t N, typename A, typename...Fields>
-		struct IndexForType;
-
-		template<size_t N, typename A, typename...Fields>
-		struct IndexForType<N, A, A, Fields...> {
-			static constexpr size_t value = N;
-		};
-
-		template<size_t N, typename A, typename B, typename...Fields>
-		struct IndexForType<N, A, B, Fields...> {
-			static constexpr size_t value = IndexForType<N + 1, A, Fields...>::value;
-		};
-	}
-
-	template <typename T, typename... Args>
-	constexpr size_t index_for_type_v = detail::IndexForType<0, T, Args...>::value;
 
 	template <size_t index, typename... Args>
 	using type_by_index = std::tuple_element_t<index, std::tuple<Args...>>;
+
+	template <typename T, typename... Args>
+	[[nodiscard]] constexpr std::optional<size_t> index_for_type() noexcept
+	{
+		std::optional<size_t> index;
+		static_for<0, sizeof...(Args)>([&index](auto i) {
+			if constexpr (std::is_same_v<T, type_by_index<static_cast<size_t>(decltype(i){}), Args... >>)
+				index = static_cast<size_t>(decltype(i){});
+		});
+
+		return index;
+	}
+
+	template <typename T, typename... Args, typename = std::enable_if_t<index_for_type<T, Args...>().has_value(), size_t>>
+	constexpr size_t index_for_type_v = *index_for_type<T, Args...>();
+
+	template <typename T, typename... Args>
+	constexpr bool has_type_v = index_for_type<T, Args...>().has_value();
+
+	template <typename T, typename... Args>
+	[[nodiscard]] constexpr size_t type_count() noexcept
+	{
+		size_t count = 0;
+		static_for<0, sizeof...(Args)>([&count](auto i) {
+			if constexpr (std::is_same_v < T, type_by_index < static_cast<size_t>(decltype(i){}), Args... >> )
+				++count;
+		});
+
+		return count;
+	}
 
 	template <typename... Args>
 	using first_type = type_by_index<0, Args...>;
 
 	template <size_t index, typename... Args>
-	constexpr auto value_by_index(Args&&... args) noexcept {
+	[[nodiscard]] constexpr auto value_by_index(Args&&... args) noexcept {
 		return std::get<index>(std::forward_as_tuple(std::forward<Args>(args)...));
 	}
 
 	template <typename Functor, class Arg, class... Args>
-	void apply(Functor&& f, Arg&& a, Args&&... args) noexcept {
+	constexpr void apply(Functor&& f, Arg&& a, Args&&... args) noexcept {
 		f(std::forward<Arg>(a));
 		if constexpr (sizeof...(args) > 0)
 			apply(std::forward<Functor>(f), std::forward<Args>(args)...);
 	}
 
 	template <typename... Args, typename Functor>
-	void for_type(Functor&& f) noexcept {
+	constexpr void for_type(Functor&& f) noexcept {
 		static_for<0, sizeof...(Args)>([f{ std::forward<Functor>(f) }](auto i) {
 			f(type_wrapper<type_by_index<decltype(i)::value, Args...>>{});
 		});
