d1f247289d1c1ee7743c72a46d5fa079c1f30add
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-04-16 23:12:03 +0300

    extra_type_traits.hpp: is_trivially_serializable_v

--- utility/extra_type_traits.hpp
+++ utility/extra_type_traits.hpp
@@ -2,13 +2,20 @@
 
 #include <type_traits>
 
-namespace detail {
-template <class Class, typename FieldType>
-constexpr FieldType member_type_from_ptr(FieldType Class::*) {return {};}
-}
+template <typename T>
+struct is_trivially_serializable {
+	using BaseT = std::remove_cv_t<T>;
 
-template <auto memberPointer>
-using member_type_from_ptr_t = decltype(detail::member_type_from_ptr(memberPointer));
+	static constexpr bool value =
+		!std::is_pointer_v<T> &&
+		!std::is_reference_v<BaseT> &&
+		std::is_same_v<BaseT, std::decay_t<BaseT>> &&
+		std::is_trivially_copyable_v<BaseT> &&
+		std::is_standard_layout_v<BaseT>;
+};
+
+template <typename T>
+constexpr bool is_trivially_serializable_v = is_trivially_serializable<T>::value;
 
 template <typename T>
 using remove_cv_and_reference_t = std::remove_cv_t<std::remove_reference_t<std::remove_cv_t<T>>>;
@@ -18,3 +25,11 @@ inline constexpr bool is_specialization_of_v = false;
 
 template<template<class...> class T, class... Args>
 inline constexpr bool is_specialization_of_v<T<Args...>, T> = true;
+
+namespace detail {
+	template <class Class, typename FieldType>
+	constexpr FieldType member_type_from_ptr(FieldType Class::*) { return {}; }
+}
+
+template <auto memberPointer>
+using member_type_from_ptr_t = decltype(detail::member_type_from_ptr(memberPointer));
--- utility/memory_cast.hpp
+++ utility/memory_cast.hpp
@@ -1,17 +1,16 @@
 #pragma once
 
+#include "../utility/extra_type_traits.hpp"
+
 #include <memory>
 #include <string.h>
-#include <type_traits>
 #include <utility>
 
 template <typename TargetType, typename SourceType, typename... TargetType_Constructor_Arguments>
 constexpr TargetType memory_cast(SourceType source, TargetType_Constructor_Arguments&& ...constructorArguments) noexcept
 {
-	static_assert(!std::is_reference_v<TargetType>);
-	static_assert(!std::is_reference_v<SourceType>);
-	static_assert(std::is_same_v<TargetType, std::decay_t<TargetType>>);
-	static_assert(std::is_trivially_copyable_v<TargetType>);
+	static_assert((!std::is_pointer_v<std::remove_cv_t<SourceType>> && is_trivially_serializable_v<SourceType>) || (std::is_pointer_v< std::remove_cv_t<SourceType>> && is_trivially_serializable_v<std::remove_pointer_t<std::remove_cv_t<SourceType>>>));
+	static_assert(is_trivially_serializable_v<TargetType>);
 
 	TargetType value{std::forward<TargetType_Constructor_Arguments>(constructorArguments)...};
 	if constexpr (std::is_pointer_v<SourceType>)
