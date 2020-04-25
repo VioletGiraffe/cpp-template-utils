9cd47783f41f2480396b2f5e634e9fcd9fce6b2a
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-04-25 21:42:04 +0300

    attempt at implementing bitwise_equal

--- utility/memory_cast.hpp
+++ utility/memory_cast.hpp
@@ -23,3 +23,20 @@ constexpr TargetType memory_cast(SourceType source, TargetType_Constructor_Argum
 
 	return value;
 }
+
+template <typename T1, typename T2>
+constexpr bool bitwise_equal(T1 v1, T2 v2)
+{
+	static_assert(sizeof(T1) == sizeof(T2));
+	static_assert(sizeof(T1) <= 8, "This is odd");
+	static_assert(is_trivially_serializable_v<T1> && is_trivially_serializable_v<T2>);
+
+	bool equal = true;
+	static_for<0, sizeof(T1)>([&](auto byteNumber) {
+		const int byteIndex = byteNumber;
+		if (((v1 >> byteIndex) & 0xFF) != ((v2 >> byteIndex) & 0xFF))
+			equal = false;
+	});
+
+	return equal;
+}
