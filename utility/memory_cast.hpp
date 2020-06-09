486c025c5081f391ae512d465da99107be6af09e
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-06-09 19:24:53 +0300

    More explicit precondition checking in memory_cast()

--- utility/memory_cast.hpp
+++ utility/memory_cast.hpp
@@ -10,7 +10,7 @@ template <typename TargetType, typename SourceType, typename... TargetType_Const
 constexpr TargetType memory_cast(SourceType source, TargetType_Constructor_Arguments&& ...constructorArguments) noexcept
 {
 	static_assert((!std::is_pointer_v<std::remove_cv_t<SourceType>> && is_trivially_serializable_v<SourceType>) || (std::is_pointer_v< std::remove_cv_t<SourceType>> && is_trivially_serializable_v<std::remove_pointer_t<std::remove_cv_t<SourceType>>>));
-	static_assert(is_trivially_serializable_v<TargetType>);
+	static_assert(!std::is_pointer_v<TargetType> && is_trivially_serializable_v<TargetType>);
 
 	TargetType value{std::forward<TargetType_Constructor_Arguments>(constructorArguments)...};
 	if constexpr (std::is_pointer_v<SourceType>)
