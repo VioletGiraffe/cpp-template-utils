b566b48ceb9152de1fcd46894619d4112ebe01d7
VioletGiraffe <noreply@github.com>
2020-07-25 21:35:06 +0300

    tuple_helpers made noexcept

--- tuple/tuple_helpers.hpp
+++ tuple/tuple_helpers.hpp
@@ -10,19 +10,19 @@
 namespace tuple {
 	namespace detail {
 		template <class Tuple, class F, size_t I>
-		constexpr void invoke(Tuple&& tuple, F f)
+		constexpr void invoke(Tuple&& tuple, F f) noexcept
 		{
 			f(std::get<I>(tuple));
 		}
 
 		template <class Tuple, class F, size_t... I>
-		[[nodiscard]] constexpr auto make_functor(std::index_sequence<I...>)
+		[[nodiscard]] constexpr auto make_functor(std::index_sequence<I...>) noexcept
 		{
 			return std::array<void (*)(Tuple &, F), sizeof...(I)>{invoke<Tuple, F, I>...};
 		}
 
 		template <class Tuple, class F, size_t... I>
-		[[nodiscard]] constexpr auto make_functor_const(std::index_sequence<I...>)
+		[[nodiscard]] constexpr auto make_functor_const(std::index_sequence<I...>) noexcept
 		{
 			return std::array<void (*)(const Tuple &, F), sizeof...(I)>{invoke<const Tuple, F, I>...};
 		}
@@ -32,7 +32,7 @@ namespace tuple {
 	inline constexpr size_t tuple_size_v_omnivorous = std::tuple_size_v<std::remove_reference_t<Tuple>>;
 
 	template <class Tuple, class F>
-	constexpr void visit(Tuple&& t, size_t index, F f)
+	constexpr void visit(Tuple&& t, size_t index, F f) noexcept
 	{
 		if constexpr (std::is_const_v<Tuple>)
 		{
@@ -47,18 +47,18 @@ namespace tuple {
 	}
 
 	template <typename T, typename... Args>
-	[[nodiscard]] constexpr size_t indexForType(const std::tuple<Args...>&) {
+	[[nodiscard]] constexpr size_t indexForType(const std::tuple<Args...>&) noexcept {
 		return ::pack::index_for_type_v<T, Args...>;
 	}
 
 	template <class Tuple>
-	[[nodiscard]] constexpr size_t size(Tuple&&)
+	[[nodiscard]] constexpr size_t size(Tuple&&) noexcept
 	{
 		return tuple_size_v_omnivorous<Tuple>;
 	}
 
 	template<class Tuple, class F>
-	void for_each(Tuple&& tuple, F func) {
+	void for_each(Tuple&& tuple, F func) noexcept {
 		static_for<0, tuple_size_v_omnivorous<Tuple>>([&](auto index_wrapper){
 			::tuple::visit(tuple, index_wrapper, func);
 		});
