b2b8a120b8c5d5d870e3bf945307835a341e6f1d
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-07-26 13:33:14 +0300

    constexpr_for: an option to terminate iteration early at runtime

--- utility/constexpr_algorithms.hpp
+++ utility/constexpr_algorithms.hpp
@@ -1,22 +1,28 @@
 #pragma once
 #include "template_magic.hpp"
 
-#include <functional> // std::less
+#include <type_traits>
 #include <utility>
-#include <vector> // std::size
 
 template <int First, int Last, typename Functor>
 constexpr void static_for([[maybe_unused]] Functor&& f) noexcept
 {
 	if constexpr (First < Last)
 	{
-		f(value_as_type<First>{});
+		if constexpr (std::is_same_v<bool, std::invoke_result_t<Functor()>>)
+		{
+			if (f(value_as_type<First>{}) == false)
+				return;
+		}
+		else
+			f(value_as_type<First>{});
+
 		static_for<First + 1, Last, Functor>(std::forward<Functor>(f));
 	}
 }
 
 template <int First, int Last, typename Functor>
-constexpr void constexpr_for([[maybe_unused]] Functor&& f) noexcept
+constexpr void constexpr_for(Functor&& f) noexcept
 {
 	static_for<First, Last>(std::forward<Functor>(f));
 }
