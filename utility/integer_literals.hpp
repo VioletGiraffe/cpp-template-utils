77c65740a6ea8fc6ce46df24a6ca7c0df44f8a90
VioletGiraffe <alexxxx89@ya.ru>
2021-04-02 15:21:59 +0300

    integer_literals.hpp compilation fixed

--- utility/integer_literals.hpp
+++ utility/integer_literals.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h> // size_t
 
-inline constexpr uint64_t operator "" _u64(unsigned long long int value)
+[[nodiscard]] inline constexpr uint64_t operator "" _u64(unsigned long long value)
 {
 	return static_cast<uint64_t>(value);
 }
 
-inline constexpr size_t operator "" _z(size_t value)
+[[nodiscard]] inline constexpr size_t operator "" _z(unsigned long long value)
 {
-	return static_cast<uint64_t>(value);
+	return static_cast<size_t>(value);
 }
