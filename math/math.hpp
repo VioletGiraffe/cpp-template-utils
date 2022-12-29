14d631e1a5356b4984bcb5da89b3e16638234682
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2022-12-29 15:14:35 +0200

    math.hpp: noexcept

--- math/math.hpp
+++ math/math.hpp
@@ -11,7 +11,7 @@
 namespace Math {
 
 template <typename T>
-T round(T value, int numDecimalDigits)
+T round(T value, int numDecimalDigits) noexcept
 {
 	static_assert(std::is_floating_point<T>::value, "This function is only intended for floating-point values");
 	const T factor = pow(T(10), T(numDecimalDigits));
@@ -20,7 +20,7 @@ T round(T value, int numDecimalDigits)
 }
 
 template <typename T>
-T floor(T value, int numDecimalDigits)
+T floor(T value, int numDecimalDigits) noexcept
 {
 	static_assert(std::is_floating_point<T>::value, "This function is only intended for floating-point values");
 	const T factor = pow(T(10), T(numDecimalDigits));
@@ -29,35 +29,35 @@ T floor(T value, int numDecimalDigits)
 }
 
 template <typename OutType, typename InType>
-constexpr typename std::enable_if_t<std::is_integral<InType>::value, OutType> floor(InType value)
+constexpr typename std::enable_if_t<std::is_integral<InType>::value, OutType> floor(InType value) noexcept
 {
 	static_assert(std::is_integral<InType>::value, "This function is only intended for integer values");
 	return static_cast<OutType>(value);
 }
 
 template <typename OutType, typename InType>
-typename std::enable_if_t<std::is_floating_point<InType>::value, OutType> floor(InType value)
+typename std::enable_if_t<std::is_floating_point<InType>::value, OutType> floor(InType value) noexcept
 {
 	static_assert(std::is_floating_point<InType>::value, "This function is only intended for floating-point values");
 	return static_cast<OutType>(::floor(value));
 }
 
 template <typename OutType, typename InType>
-constexpr typename std::enable_if<std::is_integral<InType>::value, OutType>::type round(InType value)
+constexpr typename std::enable_if<std::is_integral<InType>::value, OutType>::type round(InType value) noexcept
 {
 	static_assert(std::is_integral<InType>::value, "This function is only intended for integer values");
 	return static_cast<OutType>(value);
 }
 
 template <typename OutType, typename InType>
-typename std::enable_if<std::is_floating_point<InType>::value && std::is_floating_point<OutType>::value, OutType>::type round(InType value, bool performRounding = true)
+typename std::enable_if<std::is_floating_point<InType>::value && std::is_floating_point<OutType>::value, OutType>::type round(InType value, bool performRounding = true)  noexcept
 {
 	static_assert(std::is_floating_point<InType>::value, "This function is only intended for floating-point values");
 	return performRounding ? static_cast<OutType>(::round(value)) : static_cast<OutType>(value);
 }
 
 template <typename OutType, typename InType>
-constexpr typename std::enable_if<std::is_integral<OutType>::value && std::is_floating_point<InType>::value, OutType>::type round(InType value)
+constexpr typename std::enable_if<std::is_integral<OutType>::value && std::is_floating_point<InType>::value, OutType>::type round(InType value)  noexcept
 {
 	static_assert(std::is_floating_point<InType>::value, "This function is only intended for floating-point values");
 	const OutType integer = OutType(value + InType(0.5));
@@ -65,14 +65,14 @@ constexpr typename std::enable_if<std::is_integral<OutType>::value && std::is_fl
 }
 
 template <typename OutType, typename InType>
-OutType ceil(InType value)
+OutType ceil(InType value) noexcept
 {
 	return (OutType) ::ceil(value);
 }
 
 // Integer abs
 template <typename T>
-constexpr typename std::enable_if<std::is_integral<T>::value, T>::type abs(T value)
+constexpr typename std::enable_if<std::is_integral<T>::value, T>::type abs(T value) noexcept
 {
 	if (value > std::numeric_limits<T>::min())
 		return value >= 0 ? value : -value;
@@ -82,37 +82,37 @@ constexpr typename std::enable_if<std::is_integral<T>::value, T>::type abs(T val
 
 // Floating-point abs
 template <typename T>
-typename std::enable_if<std::is_floating_point<T>::value, T>::type abs(T value)
+typename std::enable_if<std::is_floating_point<T>::value, T>::type abs(T value) noexcept
 {
 	return ::fabs(value);
 }
 
 template<typename T>
-constexpr T clamp(T lowerBoundary, T value, T upperBoundary)
+constexpr T clamp(T lowerBoundary, T value, T upperBoundary) noexcept
 {
 	return value > upperBoundary ? value : (value < lowerBoundary ? lowerBoundary : value);
 }
 
 template <typename T>
-constexpr T signum(T value)
+constexpr T signum(T value) noexcept
 {
 	return (value > 0) ? T(1) : ((value < 0) ? T(-1) : T(0));
 }
 
 template <typename T, typename ResultType = T>
-constexpr ResultType squared(T value)
+constexpr ResultType squared(T value) noexcept
 {
 	return (ResultType)value * (ResultType)value;
 }
 
 template <typename T>
-constexpr bool isInRange(const T value, const T lowerBound, const T upperBound)
+constexpr bool isInRange(const T value, const T lowerBound, const T upperBound) noexcept
 {
 	return value >= lowerBound && value <= upperBound;
 }
 
 template <typename ResultType, typename... Args>
-constexpr ResultType arithmeticMean(Args&&... args)
+constexpr ResultType arithmeticMean(Args&&... args) noexcept
 {
 	ResultType acc = ResultType(0);
 	size_t n = 0;
@@ -126,7 +126,7 @@ constexpr ResultType arithmeticMean(Args&&... args)
 }
 
 template <typename ResultType, typename... Args>
-constexpr ResultType geometricMean(Args&&... args)
+constexpr ResultType geometricMean(Args&&... args) noexcept
 {
 	ResultType acc = ResultType(1);
 	size_t n = 0;
@@ -137,6 +137,12 @@ constexpr ResultType geometricMean(Args&&... args)
 	}, std::forward<Args>(args)...);
 
 	return (ResultType)pow(acc, 1.0 / n);
+}
+
+template <typename ResultType, typename... Args>
+constexpr ResultType geometricMean(Args&&... args) noexcept
+{
+
 }
 
 }
