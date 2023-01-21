aaecdff25395aa4105002297ee5b4f87b9ca979d
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2023-01-21 23:47:19 +0200

    More integer literals

--- utility/integer_literals.hpp
+++ utility/integer_literals.hpp
@@ -12,3 +12,18 @@
 {
 	return static_cast<size_t>(value);
 }
+
+[[nodiscard]] inline constexpr int64_t operator "" _i64(unsigned long long value)
+{
+	return static_cast<int64_t>(value);
+}
+
+[[nodiscard]] inline constexpr uint16_t operator "" _u16(unsigned long long value)
+{
+	return static_cast<uint16_t>(value);
+}
+
+[[nodiscard]] inline constexpr int16_t operator "" _i16(unsigned long long value)
+{
+	return static_cast<int16_t>(value);
+}
