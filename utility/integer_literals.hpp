fb5626f28beaef1dc2210f5a64b0c8953d8cb8c2
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-03-11 19:10:04 +0200

    Deprecation warning fixed

--- utility/integer_literals.hpp
+++ utility/integer_literals.hpp
@@ -3,27 +3,27 @@
 #include <stdint.h>
 #include <stddef.h> // size_t
 
-[[nodiscard]] inline constexpr uint64_t operator "" _u64(unsigned long long value)
+[[nodiscard]] inline constexpr uint64_t operator ""_u64(unsigned long long value)
 {
 	return static_cast<uint64_t>(value);
 }
 
-[[nodiscard]] inline constexpr size_t operator "" _z(unsigned long long value)
+[[nodiscard]] inline constexpr size_t operator ""_z(unsigned long long value)
 {
 	return static_cast<size_t>(value);
 }
 
-[[nodiscard]] inline constexpr int64_t operator "" _i64(unsigned long long value)
+[[nodiscard]] inline constexpr int64_t operator ""_i64(unsigned long long value)
 {
 	return static_cast<int64_t>(value);
 }
 
-[[nodiscard]] inline constexpr uint16_t operator "" _u16(unsigned long long value)
+[[nodiscard]] inline constexpr uint16_t operator ""_u16(unsigned long long value)
 {
 	return static_cast<uint16_t>(value);
 }
 
-[[nodiscard]] inline constexpr int16_t operator "" _i16(unsigned long long value)
+[[nodiscard]] inline constexpr int16_t operator ""_i16(unsigned long long value)
 {
 	return static_cast<int16_t>(value);
 }
