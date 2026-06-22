220c087c9df00bffd9b5125e63b3f05b885fcec1
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-06-22 14:53:12 +0300

    CallbackCaller: fixed the "use after move-from" when there is more than 1 subscriber

--- utility/callback_caller.hpp
+++ utility/callback_caller.hpp
@@ -21,8 +21,16 @@ public:
 	template <typename MethodPointer, typename ...Args>
 	void invokeCallback(MethodPointer methodPtr, Args... args) const
 	{
-		for (Interface* subscriber: _subscribers)
-			(subscriber->*methodPtr)(std::forward<Args>(args)...);
+		// args are already by-value copies, not forwarding references, so std::forward would move out of them. Only the
+		// last subscriber can safely receive the moved value - every earlier one still needs args intact for the next call.
+		const size_t nSubscribers = _subscribers.size();
+		if (nSubscribers == 0)
+			return;
+
+		for (size_t i = 0; i < nSubscribers - 1; ++i)
+			(_subscribers[i]->*methodPtr)(args...);
+
+		(_subscribers[nSubscribers - 1]->*methodPtr)(std::forward<Args>(args)...);
 	}
 
 protected:
