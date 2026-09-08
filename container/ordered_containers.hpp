7adad3c5644d2615c2572e7fd61f20eb2a13a9fc
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-09-09 02:15:51 +0300

    ordered_container fixes

--- container/ordered_containers.hpp
+++ container/ordered_containers.hpp
@@ -10,22 +10,37 @@ class ordered_container : public ContainerType
 {
 public:
 	template <typename T>
-	typename ContainerType::iterator find(const T& value);
+	[[nodiscard]] typename ContainerType::iterator find(const T& value);
+
+	template <typename T>
+	[[nodiscard]] typename ContainerType::const_iterator find(const T& value) const;
 
 	void sort();
 
 private:
-	bool _sorted = false;
+	// Shared by both find() overloads: SelfType carries the constness into the iterator type
+	template <typename SelfType, typename T>
+	static auto find_impl(SelfType& self, const T& value)
+	{
+		const auto end_iterator = self.end();
+		// Verified rather than tracked: mutations through the public base cannot be intercepted
+		assert(std::is_sorted(self.begin(), end_iterator, Comparator()));
+
+		const auto it = std::lower_bound(self.begin(), end_iterator, value, Comparator());
+		return it != end_iterator && Comparator()(value, *it) == false ? it : end_iterator;
+	}
 };
 
 template <class ContainerType, typename Comparator> template <typename T>
-typename ContainerType::iterator ordered_container<ContainerType, Comparator>::ordered_container::find(const T& value)
+typename ContainerType::iterator ordered_container<ContainerType, Comparator>::find(const T& value)
 {
-	assert(_sorted);
+	return find_impl(*this, value);
+}
 
-	const auto end_iterator = ContainerType::end();
-	const auto it = std::lower_bound(ContainerType::begin(), end_iterator, value, Comparator());
-	return it != end_iterator && Comparator()(value, *it) == false ? it : end_iterator;
+template <class ContainerType, typename Comparator> template <typename T>
+typename ContainerType::const_iterator ordered_container<ContainerType, Comparator>::find(const T& value) const
+{
+	return find_impl(*this, value);
 }
 
 
@@ -33,5 +48,4 @@ template <class ContainerType, typename Comparator /*= std::less<> */>
 void ordered_container<ContainerType, Comparator>::sort()
 {
 	std::sort(ContainerType::begin(), ContainerType::end(), Comparator());
-	_sorted = true;
 }
